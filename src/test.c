21d2c0eabc8004fbc4a179b14139bbdacff3b11b
Jasper Shovelton <Beanie.github@proton.me>
2023-06-24 15:33:37 +0100

    use lorem ipsum like a normal person

--- src/test.c
+++ src/test.c
@@ -3,8 +3,20 @@
 int main() {
 	
 	for (int i = 0; i < 1000000; i++) {
-		char string1[] = "So, you want to get good at spy? Here is a comprehensive guide on gittin\' gud, as the kiddos say. First of all, you need to master face stabs. You do this by typing \'cl_interp five million\' into the console. This will have the side effect of everyone hating you, but it\'s all worth it for that sweet sweet green \'CRITICAL HIT!!!\' above your opponents\' heads when you click on their face. Next, you need to get good. This does mean that you have to spend a couple of hundred hours on spy, but it's worth it for the constant angry people in chat accusing you of facestabs. They are right, but who cares.";
-		char string2[] = " gfhio vordpz gy lrd zb uirp vgyi rg yl nubotu olghurilehshogbures hvidx; bvhfdx hubrdol hgiglrbsvhfidk vbnufidon vhufdlxbng5750j ap;v f;alvfnlhadlo";
+		char string1[] = "Lorem ipsum dolor sit amet, consectetur "
+			"adipiscing elit. Proin dignissim enim quis justo "
+			"posuere tincidunt eu eget dolor. Vivamus a tortor et "
+			"orci pellentesque convallis ac nec ipsum. Proin "
+			"interdum mattis purus, ac iaculis tortor dapibus id. "
+			"Nam et sem rutrum, luctus mauris eu, lobortis orci. "
+			"Nulla ultrices dignissim maximus. Morbi risus odio, "
+			"vulputate sit amet mollis sit amet, condimentum id "
+			"metus. Aenean eu ultrices nulla. Praesent nec quam "
+			"odio. Vestibulum non neque risus.";
+		char string2[] = "V4ysUlCSrBXE0barIE5vtNYhDpNnHPWJlHbJ2Qukp7hy"
+			"MTRqsHUBie0vHobEL6KbOOd2lJRMVoJQP5lmyioGqzhNciare9Jio"
+			"EbY04nN0GNmIgSWxN6w1tKq72WFhqK4GbfSCj3VRsdfKDUFS3RKCe"
+			"nYo6awvwLXVz6ckIMxthU=";
 		squeeze(string1, string2);
 	}
 
